4bba5050b28d0f3c126d94ec69794ba1be7f86b1
Harsh <harshguptaaa17@gmail.com>
2023-08-22 02:15:30 +0530

    hero section complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,15 +13,20 @@ export default function Home() {
       </ul>
     <p className='pr-10'>Add to Cart</p>
     </nav>
-    <div className='border-2 border-black h-96 mt-10 grid grid-cols-3 w-[85%] mx-auto'>
+    <div className='border-2 border-black mt-10 grid grid-cols-3 w-[85%] mx-auto py-10 '>
       <div>
-        <h1 className='font-black text-6xl max-w-[75%] mx-auto font-ubuntu pl-10'>
+        <h1 className='font-black text-6xl max-w-[75%] mx-auto font-ubuntu'>
           Elegance is good taste, plus a dash of daring.</h1>
-        <button className='bg-black text-white rounded-3xl px-6 py-3 mx-auto block mt-10'>Shop Now</button>
-        <button><ChevronDown/></button>
+        <button className='bg-black text-white rounded-3xl px-6 py-3 ml-12 block mt-10'>Shop Now</button>
+        <button className='pl-12 mt-16 animate-bounce'><ChevronDown className='w-10 h-10'/></button>
       </div>
-      <h1>Gand</h1>
-      <h1>Mrao</h1>
+      <div >
+        <Image width={400} height={400} className='border-black border-2' src="/heroimg.png" alt="" />
+        <p className='max-w-[80%] mt-5 text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti laborum, unde enim veniam officiis neque</p>
+      </div>
+      <div>
+      <Image width={400} height={400} className='border-black border-l-2 border-r-2 border-b-2' src="/heroimg2.png" alt="" />
+      </div>
     </div>
     </main>
   )
