c1a079ce203b3f07a898c1d041bf9b19798556f0
Javed Ansari <javedans2003@gmail.com>
2023-08-27 20:37:38 +0530

    postinstall script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,66 @@
-import Image from 'next/image'
-import {ChevronDown, ShoppingBag} from "lucide-react"
-import New from './New';
-import Collection from './Collection';
+import Image from "next/image";
+import { ChevronDown, ShoppingBag } from "lucide-react";
+import New from "../components/New";
+import Collection from "../components/Collection";
+import NavBar from "@/components/nav-bar";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="font-bold">
-    <nav className='flex justify-between mx-20 pt-8 border-b-[0.01px] pb-8 border-black'>
-      <ul className='flex gap-10 pl-10'>
-        <li>Home</li>
-        <li>About Us</li>
-        <li>Shop</li>
-        <li>Blog</li>
-      </ul>
-    <button className='pr-10 flex gap-3'>
-      <ShoppingBag className='w-6 h-6'/>
-      <p>Add to Cart</p>
-    </button>
-    </nav>
-    <div className='border-b-[0.01px] border-black mt-10 grid grid-cols-3 w-[85%] mx-auto pt-10 pb-16 mb-10 relative'>
-      <div>
-        <h1 className='font-black text-6xl max-w-[75%] mx-auto font-ubuntu'>
-          Elegance is good taste, plus a dash of daring.</h1>
-        <button className='bg-black text-white rounded-3xl px-6 py-3 ml-12 block mt-10'>Shop Now</button>
-        <button className='pl-24 mt-20 animate-bounce'><ChevronDown className='w-10 h-10'/></button>
-      </div>
-      <div >
-        <Image width={400} height={300} className='border-black border-2' src="/heroimg.png" alt="" />
-        <p className='max-w-[80%] mt-5 text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti laborum, unde enim veniam officiis neque</p>
-      </div>
-      <div>
-      <Image width={400} height={400} className='border-black border-l-2 border-r-2 border-b-2' src="/heroimg2.png" alt="" />
-      </div>
-      <div className='flex gap-10 bg-[#f8f8f8] absolute bottom-0 translate-y-1/2 left-[50%] -translate-x-1/2 px-10'>
-        <li>Facebook</li>
-        <li>Instagram</li>
-        <li>X</li>
-      </div>
-    </div>
-    <New/>
-    <Collection/>
-    </main>
-  )
+    <>
+      <main className="grid place-items-center">
+        <div className="grid place-items-center pt-7 ">
+          <Image
+            src={"/hero.webp"}
+            width={1920}
+            height={1080}
+            alt="hero"
+            className=""
+          />
+        </div>
+        <section className=" relative text-center  text-7xl font-black text-gray-700 py-16  tracking-widest">
+          <Link href={"/"}>Grab the latest collection.</Link>
+          <Image
+            src={"/group.svg"}
+            width={100}
+            height={100}
+            alt="markerstroke"
+            className="absolute top-2 -right-20 -z-10 object-contain  "
+          />
+          
+        </section>
+        <div className="grid grid-cols-4 gap-10 ">
+          <Image
+            src={"/model1.webp"}
+            width={500}
+            height={500}
+            alt="model1"
+            className="group-hover:rounded-full duration-500"
+          />
+
+          <Image
+            src={"/model2.webp"}
+            width={500}
+            height={500}
+            alt="model2"
+            className=""
+          />
+          <Image
+            src={"/model3.webp"}
+            width={500}
+            height={500}
+            alt="model3"
+            className=""
+          />
+          <Image
+            src={"/model4.webp"}
+            width={500}
+            height={500}
+            alt="model4"
+            className=""
+          />
+        </div>
+      </main>
+    </>
+  );
 }
---
+++ src/components/nav-bar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { ShoppingBag } from "lucide-react";
+import Link from "next/link";
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav className="flex justify-between  pt-8 border-b-[0.01px] pb-8 border-black">
+        <ul className="flex gap-10 pl-10">
+          <Link href="/">Home</Link>
+          <Link href="/About">About</Link>
+          <Link href="/Shop">shop</Link>
+          <Link href="/Contact">Contact</Link>
+        </ul>
+        <button className="pr-10 flex gap-3">
+          <ShoppingBag className="w-6 h-6" />
+          <p>Add to Cart</p>
+        </button>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
