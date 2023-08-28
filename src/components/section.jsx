96fbd21e8da70c6051c98dfadfbb1c58dbd36573
Javed Ansari <javedans2003@gmail.com>
2023-08-28 23:11:55 +0530

    images

---
+++ src/components/section.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Section = () => {
+  return (
+    <div className='grid grid-cols-2 w-full gap-10 my-96'>
+        <Image src={"/sidel.webp"} alt={"Sidel"} width={700} height={500} className=' object-contain' />
+        <Image src={"/sider.webp"} alt={"Sidel"} width={700} height={500} className=' object-contain' />
+    </div>
+  )
+}
+
+export default Section
